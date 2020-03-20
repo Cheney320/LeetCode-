930758355841d13992499707b1f4bd7a7a9a9ff4
hc_data <1197063125@qq.com>
2020-03-21 00:17:57 +0800

    update day11

---
+++ day11/105.从前序与中序遍历序列构造二叉树.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode.cn id=105 lang=python3
+#
+# [105] 从前序与中序遍历序列构造二叉树
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+# @lc code=end
+
